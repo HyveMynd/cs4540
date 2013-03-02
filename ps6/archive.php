13a8e6cb55eb8c24eee3a3c30c0014b68f2ebd48
Andres <andresf.mon@gmail.com>
2013-03-02 09:45:36 -0700

    added reg form example

---
+++ ps6/archive.php
@@ -0,0 +1,36 @@
+<?php
+if(!isset($_SESSION))
+	session_start();
+require 'functions.php';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" href="style.css" type="text/css" />
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" type="text/javascript"></script>
+<script src=resume.js type="text/javascript"></script>
+<title>Employment History</title>
+</head>
+<body class='archive'>
+	<h1>Archive Resume</h1>
+	<form method="post" action="Switch.php">
+	<table>
+		<tr>
+			<td>Resume Name</td>
+			<td><input id="resName" name="name" type="text" value="<?php sticky('resumeName')?>" maxlength="20" required /></td>
+			<td><input type="submit" name=submit value="Store" /></td>
+			<td><input type="submit" name=submit value="Load" /></td>
+			<td><input type="submit" name=submit value="Delete" /></td>
+			<td><input type="submit" name=submit value="View" /></td>
+		</tr>
+	</table>
+	</form>
+	<p><a href="contactInfo.php" >Contact Info</a></p>
+	<p><a href="positionSought.php" >Position Sought</a></p>
+	<p><a href="employmentHistory.php" >Employment History</a></p>
+	<p><a href="viewResume.php" >View Resume</a></p>
+</body>
+<script type="text/javascript" >
+
+</script>

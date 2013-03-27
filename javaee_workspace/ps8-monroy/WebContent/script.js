4a287707217ced61c924f8cf11719179226bca36
Andres <andresf.mon@gmail.com>
2013-03-26 19:41:01 -0600

    added ps8

---
+++ javaee_workspace/ps8-monroy/WebContent/script.js
@@ -0,0 +1,20 @@
+function ServerCall(url){
+	$.ajax({
+		url:url,
+		success:func(result),
+		data:"",
+		type:"GET"
+	});
+}
+
+function loadFirstTenFromLibrary(){
+	
+}
+
+function loadNextTenFromLibrary(){
+	
+}
+
+function loadPreviousTenFromLibarary(){
+	
+}
